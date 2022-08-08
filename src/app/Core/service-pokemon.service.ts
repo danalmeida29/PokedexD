3e9a52c31a0754d535f13458a5e3eb395c4d843c
Daniel <daniel.m.almeida.97@gmail.com>
2022-08-08 18:06:44 -0300

    configurandoTipo
    Configurando o metodo getPokemon

--- src/app/Core/service-pokemon.service.ts
+++ src/app/Core/service-pokemon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+const BASE_URL = 'https://pokeapi.co/api/v2';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,12 @@ export class pokemonService {
   constructor(private http: HttpClient) { }
 
   getPokemon(id: any){
-    var urlString = `${BASE_URL}/${id}`;
+    var urlString = `${BASE_URL}/pokemon/${id}`;
+    return this.http.get(urlString);
+  }
+
+  getType(id: any){
+    var urlString = `${BASE_URL}/type/${id}`;
     return this.http.get(urlString);
   }
 }
--- src/app/Pages/details-pokemon/details-pokemon.component.ts
+++ src/app/Pages/details-pokemon/details-pokemon.component.ts
@@ -10,11 +10,15 @@ import { TypeColorModel } from 'src/app/Models/ModelTypeColor';
 export class DetailsPokemonComponent implements OnInit {
   namePokemon: any;
   idPokemon: any;
-  erro: any;
+
+  Erro: any;
   corPorTipo: any;
+
   personagensPokemon:any;
+  typePokemon:any;
 
   typeColor: TypeColorModel[] = [];
+  typeDamage: any;
   
   constructor(
     private servicePokemon: pokemonService,
@@ -23,6 +27,24 @@ export class DetailsPokemonComponent implements OnInit {
   ngOnInit(): void {
     this.idPokemon = localStorage.getItem('PokemonId');
     this.getPokemon();
+    this.getType();
+  }
+
+
+  getType() {
+    this.servicePokemon.getType(this.idPokemon).subscribe(
+      (res:any)=>{
+        for (let index = 0; index < this.typePokemon.damage_relations.length; index++) {
+          const element = this.typePokemon.damage_relations[index];
+
+          this.typeDamage.push(element.double_damage_from.name).toFixed;
+          console.log("o que a de novo scooby Doo?", this.typeDamage[index]);
+        }
+      },
+      (error: any) => {
+        this.Erro = error;
+      }
+    );
   }
 
   getPokemon(){
@@ -35,98 +57,100 @@ export class DetailsPokemonComponent implements OnInit {
       for (let index = 0; index < this.personagensPokemon.types.length; index++) {
         const element = this.personagensPokemon.types[index];
 
+        this.validatingColorByType(element.type.name);
 
-        switch (element.type.name) {
-
-          case "bug":
-            this.corPorTipo = "#9BBA48;"
-          break; 
-
-          case "drak":
-            this.corPorTipo = "#595761;;"
-          break; 
-
-          case "dragon":
-            this.corPorTipo = "#2C6AC1;"
-          break; 
-
-          case "eletric":
-            this.corPorTipo = "#D4BC34;"
-          break; 
-
-          case "fighting":
-            this.corPorTipo = "#C44D61;"
-          break; 
-
-          case "flying":
-            this.corPorTipo = "#758CBD;"
-          break;
-
-          case "fire":
-            this.corPorTipo = "#E96303;"
-          break; 
-
-          case "fairy":
-            this.corPorTipo = "#E296E1;"
-          break; 
-
-          case "grass":
-            this.corPorTipo = "#73B861;"
-          break;
-
-          case "ground":
-            this.corPorTipo = "#CE8056;"
-          break;
-
-          case "ghost":
-            this.corPorTipo = "#616EB7;"
-          break;
-
-          case "ice":
-            this.corPorTipo = "#71BAAC;"
-          break;
-
-          case "normal":
-            this.corPorTipo = "#909090;"
-          break;
-
-          case "poison":
-            this.corPorTipo = "#AC6ACA;"
-          break;  
-
-          case "psychic":
-            this.corPorTipo = "#EB8B85;"
-          break; 
-
-          case "rock":
-            this.corPorTipo = "#84BEB3;"
-          break;  
-
-          case "rock":
-            this.corPorTipo = "#6594A1;"
-          break; 
-
-          case "water":
-            this.corPorTipo = "#4F77BE;"
-          break; 
-
-          default:
-            this.corPorTipo = "gray"
-            break;
-        }
-
-        this.typeColor.push(element.type.name ,this.corPorTipo).toFixed;
-        console.log(this.typeColor);
+        this.typeColor.push({type: element.type.name ,color: this.corPorTipo}).toFixed;
+        console.log(this.typeColor[index].type + ' ' + this.typeColor[index].color);
       }
-      
-      
-
-      
-      
     },
     (error: any) => {
-      this.erro = error;
+      this.Erro = error;
     });
+  }
+
+  /**
+   * metodo que atribui a cor de acordo com o tipo passado como parametro
+   * @param tipo [String] : parametro que define os tipos dos pokemons.
+   */
+  validatingColorByType(tipo: string){
+    switch (tipo) {
+
+      case "bug":
+        this.corPorTipo = "#9BBA48;" 
+      break; 
+
+      case "drak":
+        this.corPorTipo = "#595761;;"
+      break; 
+
+      case "dragon":
+        this.corPorTipo = "#2C6AC1;"
+      break; 
+
+      case "eletric":
+        this.corPorTipo = "#D4BC34;"
+      break; 
+
+      case "fighting":
+        this.corPorTipo = "#C44D61;"
+      break; 
+
+      case "flying":
+        this.corPorTipo = "#758CBD;"
+      break;
+
+      case "fire":
+        this.corPorTipo = "#E96303;"
+      break; 
+
+      case "fairy":
+        this.corPorTipo = "#E296E1;"
+      break; 
+
+      case "grass":
+        this.corPorTipo = "#73B861;"
+      break;
+
+      case "ground":
+        this.corPorTipo = "#CE8056;"
+      break;
+
+      case "ghost":
+        this.corPorTipo = "#616EB7;"
+      break;
+
+      case "ice":
+        this.corPorTipo = "#71BAAC;"
+      break;
+
+      case "normal":
+        this.corPorTipo = "#909090;"
+      break;
+
+      case "poison":
+        this.corPorTipo = "#AC6ACA;"
+      break;  
+
+      case "psychic":
+        this.corPorTipo = "#EB8B85;"
+      break; 
+
+      case "rock":
+        this.corPorTipo = "#84BEB3;"
+      break;  
+
+      case "rock":
+        this.corPorTipo = "#6594A1;"
+      break; 
+
+      case "water":
+        this.corPorTipo = "#4F77BE;"
+      break; 
+
+      default:
+        this.corPorTipo = "gray"
+      break;
+    }
   }
 
 }
