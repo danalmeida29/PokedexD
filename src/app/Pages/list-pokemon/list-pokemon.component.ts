9c314552af3322335af9579955e49f2a5a99ed45
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2022-08-29 12:45:28 -0300

    implementacaoDoFilter

--- src/app/Pages/list-pokemon/list-pokemon.component.ts
+++ src/app/Pages/list-pokemon/list-pokemon.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ElementRef, Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { PokemonsModel } from 'src/app/Models/ModelPokemon';
 import { pokemonService } from 'src/app/Core/service-pokemon.service'
@@ -11,7 +11,6 @@ import { pokemonService } from 'src/app/Core/service-pokemon.service'
 
 export class ListPokemonComponent implements OnInit {
 
-  @ViewChild('filterInput') filterInput!:  ElementRef
   erro: any;
   pokemons: any;
 
@@ -22,26 +21,23 @@ export class ListPokemonComponent implements OnInit {
     private pokemonService: pokemonService
   ) { }
 
+
   ngOnInit(): void {
-    // console.log('test:');
     this.listPokemon();
   }
+
+
 
   listPokemon(){
     this.pokemons = this.pokemonService.carregarPokemons().subscribe(
       (res: any) => {
         this.pokemons = res;
-        console.log("Console do res:", this.pokemons);
         for(let i = 0; i <this.pokemons.results.length; i++){
           const pokemon = this.pokemons.results[i];
           this.pokemonList.push({name: pokemon.name, url: pokemon.url, id:i+1 }).toFixed;
         }
-        //For percorrendo cada um dos pokemon em pokemons.results
-        // dentro do for dar um push em pokemonsList, incluindo no parametro Id o index + 1
-        //--  no ngFor do HTML deve percorrer o pokemonList que tem, nome Url e Id para poder pegar a imagem passando o Id
 
-        
-        console.log('Console do listPokemon: ', this.pokemonList);
+        this.filteredList = this.pokemonList;
       },
       (error: any) => {
         this.erro = error;
@@ -49,13 +45,11 @@ export class ListPokemonComponent implements OnInit {
     );
   }
 
-  filterPokemon(){
-    let filterValue = this.filterInput.nativeElement.value
-    this.filteredList = this.pokemonList.filter((pokemons: any) =>
-    pokemons.name.trim().toLowerCase().includes(filterValue)
-    );
-
-    console.log(this.filterInput.nativeElement.value);
+  filterPokemon(event: Event){
+    let filterValue = (event.target as HTMLInputElement).value
+    this.filteredList = this.pokemonList.filter((pokemon: any) =>
+    pokemon.name.trim().toLowerCase().includes(filterValue)
+    )
   }
 
   detailsPokemons(id:any){
